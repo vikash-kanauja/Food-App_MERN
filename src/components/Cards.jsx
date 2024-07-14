f188dab01a18f91db773224a8aa5ee7e1d1b81e5
vikash-kanauja <vikashkanoujia@gmail.com>
2024-07-14 11:45:45 +0530

    add special dis section

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,23 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
+import { FaHeart } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 const Cards = ({ item }) => {
+  const [isHeartFillted,setIsHeartFillted] = useState(false);
+
+  const handleHeartClick = ()=>{
+    setIsHeartFillted(!isHeartFillted)
+  }
   return (
-    <div className="">
-      <div>
-        <div className="card bg-base-100 w-96 shadow-xl">
-  <figure>
-    <img
-      src={item.image}
-      alt="Shoes" />
-  </figure>
-  <div className="card-body">
-    <h2 className="card-title">Shoes!</h2>
-    <p>If a dog chews shoes whose shoes does he choose?</p>
-    <div className="card-actions justify-end">
-      <button className="btn btn-primary">Buy Now</button>
+    <div className="card w-96 bg-base-100 shadow-xl">
+    <div className={`rating gap-1 absolute right-2 top-2 p-4 heartStar bg-green ${isHeartFillted ? "text-rose-500" : "text-white"}`} onClick={handleHeartClick}>
+      <FaHeart className="h-5w-5 cursor-pointer"/>
     </div>
-  </div>
-</div>
+      <Link to={`/menu/${item._id}`}>
+        <figure>
+          <img 
+          src={item.image} 
+          alt="" 
+          className="hover:scale-10s transition-all duration-200 md:h-72"/>
+        </figure>
+      </Link>
+      <div className="card-body">
+        <Link to={`/menu/${item._id}`}><h2 className="card-title">{item.name}</h2></Link>
+        <p>Discription of item</p>
+        <div className="card-actions justify-between items-center mt-2">
+          <h5 className="font-semibold">
+            <span className="text-sm text-rose-400">$</span>
+            {item.price}
+          </h5>
+          <button className="btn bg-green text-white">Buy Now</button>
+        </div>
       </div>
     </div>
   );
--- src/pages/home/SpecialDishes.jsx
+++ src/pages/home/SpecialDishes.jsx
@@ -3,7 +3,33 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Slider from "react-slick";
 import Cards from "../../components/Cards";
+import { FaAngleLeft, FaAngleRight } from "react-icons/fa";
 
+const SimpleNextArrow = (props) => {
+  const { className, style, onClick } = props;
+  return (
+    <div
+      className={className}
+      style={{ ...style, display: "block", background: "red" }}
+      onClick={onClick}
+    >
+      NEXT
+    </div>
+  );
+};
+
+const SimplePrevArrow = (props) => {
+  const { className, style, onClick } = props;
+  return (
+    <div
+      className={className}
+      style={{ ...style, display: "block", background: "green" }}
+      onClick={onClick}
+    >
+      BACK
+    </div>
+  );
+};
 const SpecialDishes = () => {
   const [recipes, setRecipes] = useState([]);
   const slider = React.useRef(null);
@@ -22,8 +48,8 @@ const SpecialDishes = () => {
     dots: true,
     infinite: false,
     speed: 500,
-    slidesToShow: 4,
-    slidesToScroll: 4,
+    slidesToShow: 3,
+    slidesToScroll: 3,
     initialSlide: 0,
     responsive: [
       {
@@ -51,21 +77,35 @@ const SpecialDishes = () => {
         },
       },
     ],
+    nextArrow: <SimpleNextArrow />,
+    prevArrow: <SimplePrevArrow />,
   };
   return (
     <div>
-      <div className="section-container my-20">
+      <div className="section-container my-20 relative">
         <div className="text-left">
           <p className="subtitle"> Special Dishes</p>
           <h2 className="title md:w-[520px]">Standout Dishes From Our Menu</h2>
         </div>
-      </div>
+        <div className="md:absolute right-3 top-8 mb-10 md:mr-24">
+        
+          <button
+            onClick={() => slider?.current?.slickPrev()}
+            className="btn rounded-full ml-5" >
+            <FaAngleLeft classNane="h-8 w-8 p-1" />
+          </button>
+        
+          <button
+            onClick={() => slider?.current?.slickNext()}
+            className="btn  rounded-full ml-5">
+            <FaAngleRight classNane="h-8 w-8 p-1" />
+          </button>
 
-      <Slider {...settings}>
+        </div>
+      </div>
+      <Slider ref={slider} {...settings} className="overflow-hidden mt-10 space-x-5">
         {recipes.map((item, i) => {
-          return (
-            <Cards key={i} item={item} />
-          );
+          return <Cards key={i} item={item} />;
         })}
       </Slider>
     </div>
